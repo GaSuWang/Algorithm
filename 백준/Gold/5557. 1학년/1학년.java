7907de3cc060be389a5d442decd3ed7d3a934c70
GaSuWang <87304258+GaSuWang@users.noreply.github.com>
2022-10-08 13:28:50 +0900

    [Gold V] Title: 1학년, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int N = Integer.parseInt(br.readLine());
+    	int[] arr = new int[N];
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for(int i=0; i<N;i++) {
+    		arr[i] = Integer.parseInt(st.nextToken());
+    	}
+    	long dp[][] = new long[N][21];
+    	dp[0][arr[0]] = 1;
+    	for(int i=1; i<N;i++) {
+    		for(int j=0;j<21;j++) {
+    			if(dp[i-1][j]==0) continue;
+    			if(j+arr[i]<=20) {
+    				dp[i][j+arr[i]] += dp[i-1][j];
+//    				dp[i][j+arr[i]]
+    			}
+    			if(j-arr[i]>=0) {
+    				dp[i][j-arr[i]] += dp[i-1][j];
+    			}
+    		}
+    	}
+    	System.out.println(dp[N-2][arr[N-1]]);
+    }
+}
