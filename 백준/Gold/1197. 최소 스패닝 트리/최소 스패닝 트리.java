d8980378e31f1d12badc973cf099d604482958f0
GaSuWang <87304258+GaSuWang@users.noreply.github.com>
2022-10-22 13:19:26 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 520 ms, Memory: 56264 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,87 +1,105 @@
+
+
+////
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
+
+
+class Data implements Comparable<Data>{
+	int v,w;
+	
+	Data(int v, int w){
+		this.v = v;
+		this.w = w;
+				
+	}
+	
+	@Override
+	public int compareTo(Data o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(this.w, o.w);
+	}
+	
+}
+
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		int start, end, weight;
-
-		public Edge(int start, int end, int weight) {
-			super();
-			this.start = start;
-			this.end = end;
-			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			// TODO Auto-generated method stub
-			return this.weight-o.weight;
-		}
-		
-	}
 	
-	static int p[];
-	static int vn;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		vn = Integer.parseInt(st.nextToken());
-		int en = Integer.parseInt(st.nextToken());
-		Edge [] arr = new Edge[en];
-		for(int i=0;i<en;i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken())-1;
-			int end = Integer.parseInt(st.nextToken())-1;
-			int weight = Integer.parseInt(st.nextToken());
-			arr[i] = new Edge(start, end, weight);
-		}
-		Arrays.sort(arr);
-		makeSet();
-		int res = 0;
-		int cnt = 0;
-		for(Edge edge : arr) {
-			if(unionSet(edge.start, edge.end)) {
-				cnt++;
-				res+=edge.weight;
-			}
-			if(cnt == vn-1) {
-				break;
-			}
-		}
-		System.out.println(res);
-		
-	}
-	 static boolean unionSet(int a, int b) {
-	        a = findSet(a);
-	        b = findSet(b);
-	        if(a == b) {
-	            return false;
-	        }
-	        p[b] = a; 
-	        return true;
-	    }
-	    
-
-	    static int findSet(int a) {
-	        if(a == p[a]) { 
-	            return a;
-	        }
-	        p[a] = findSet(p[a]); 
-	        return p[a]; 
-	    }
-
-	    static void makeSet() {
-	        p = new int[vn];
-	        for(int i =0; i < vn; i++) {
-	            p[i] = i;
-	        }
-	    }
-
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int V = Integer.parseInt(st.nextToken());
+    	int E = Integer.parseInt(st.nextToken());
+    	//int map[][] = new int[V][V];
+    	List<Data> [] list = new List[V];
+//    	Arrays.fill(list, new ArrayList<Data>());
+    	boolean check[] = new boolean[V];
+//    	int minEdges[] = new int [V];
+    	PriorityQueue <Data> pq = new PriorityQueue<Data>();
+    	for(int i=0; i<E;i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int v1 = Integer.parseInt(st.nextToken())-1;
+    		int v2 = Integer.parseInt(st.nextToken())-1;
+    		int len = Integer.parseInt(st.nextToken());
+    		if(list[v1]==null) {
+    			list[v1] = new ArrayList<Data>();
+    		}
+    		if(list[v2]==null) {
+    			list[v2] = new ArrayList<Data>();
+    		}
+    		list[v1].add(new Data(v2,len));
+    		list[v2].add(new Data(v1,len));
+//    		map[v1][v2] = len;
+//  		map[v2][v1] = len;
+    	}
+    	
+//   	Arrays.fill(minEdges,Integer.MAX_VALUE);
+    	int res = 0;
+//    	minEdges[0] = 0;
+//    	for(int i=0; i<V;i++) {
+//    		int minEdge = Integer.MAX_VALUE;
+//    		int minVertex=-1;
+    	
+//    		for(int j=0; j<V;j++) {
+//    			if(check[j]) continue;
+//    			if(minEdge > minEdges[j]) {
+//    				minVertex = j;
+//    				minEdge = minEdges[j];
+//    			}
+//    		}
+    	int cnt = 0;
+    	pq.offer(new Data(0,0));
+    	while(!pq.isEmpty()) {
+//    		check[minVertex] = true;
+//    		for(int j=0; j<list[minVertex].size();j++) {
+//    			int [] cur = list[minVertex].get(j);
+//    			if(check[cur[0]]) continue;
+//    			if(cur[1]<minEdges[cur[0]]) minEdges[cur[0]] = cur[1];
+//    			if(check[j]||map[minVertex][j]==0)continue;
+//    			if(map[minVertex][j]<minEdges[j]) {
+//    				minEdges[j] = map[minVertex][j];
+//    			}
+//    		}
+    		
+    		Data cur = pq.poll();
+    		if(check[cur.v]) continue;
+    		res += cur.w;
+    		check[cur.v] = true;
+    		cnt++;
+    		if(cnt==V) {
+    			break;
+    		}
+    		for(int j=0; j<list[cur.v].size();j++) {
+    			if(check[list[cur.v].get(j).v]) continue;
+    			pq.offer(new Data(list[cur.v].get(j).v,list[cur.v].get(j).w));
+    		}
+    	}
+    	System.out.println(res);	
+    }
 }
