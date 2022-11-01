4aaadabb1bc9a4c319420c1631f4a0b1ba58a4b6
GaSuWang <87304258+GaSuWang@users.noreply.github.com>
2022-11-01 23:18:34 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 112 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -16,24 +16,6 @@ public class Main {
 			y = Long.parseLong(st.nextToken());
 			long res = 0;
 			long num = y-x;
-//			arr = new long[(int) ((num/2)+1)];
-//			int cur = 2;
-//			arr[1] = num;
-//			while(true) {
-//				if(arr[1]==1) break;
-//				if(arr[1]-1<cur) {
-//					cur--;
-//					continue;
-//				}
-//				arr[cur] = arr[cur]+1;
-//				arr[1] = arr[1] - cur;
-//				cur++;
-//			}
-//		
-//			for(int i=1;i<(int)((num/2)-1);i++) {
-//				if(arr[i]==0) break;
-//				res+=arr[i];
-//			}
 			long sum = 0;
 			int temp = 1;
 			while(sum<num) {
